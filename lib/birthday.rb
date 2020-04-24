65ae4a49c5dd1422931d3295304ee138e4518900
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 16:19:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!" if age <= 12
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!" 
+    else 
+    end
   end
 end
 
